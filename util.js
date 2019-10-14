b0437fc44361c1f7a4065add44e65142558cb1ef
Steve Gerbino <steve@gerbino.co>
2019-10-14 19:43:48 -0400

    Added the ability to add n number of token emissions

---
+++ util.js
@@ -0,0 +1,23 @@
+function append_form_elements( elements, append ) {
+   for ( i = 0; i < elements.length; i++ ) {
+      var e = elements[i];
+      if ( e.tagName == "LABEL" ) {
+         e.htmlFor += "-" + append;
+      } else if ( e.tagName == "INPUT" || e.tagName == "TEXTAREA" ) {
+         e.name += "_" + append;
+         e.id += "-" + append;
+      } else {
+         e.id += "-" + append;
+      }
+   }
+}
+
+var _token_emission_ctr = 0;
+function add_token_emission() {
+    _token_emission_ctr++;
+    var template_node = document.getElementById( "token-emission-template" ).cloneNode( true );
+    template_node.id += ( "-" + _token_emission_ctr );
+    append_form_elements( template_node.querySelectorAll('*'), _token_emission_ctr );
+    document.getElementById( "token-emissions" ).appendChild( template_node );
+}
+
