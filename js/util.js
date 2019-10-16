1deea30231b8f0061bb45949a9d9443cf247bed2
Steve Gerbino <steve@gerbino.co>
2019-10-16 17:45:55 -0400

    Create transaction as consumed by steemjs for form submission

--- js/util.js
+++ js/util.js
@@ -80,75 +80,91 @@ function onAllowVotingClicked( e ) {
 }
 
 function createToken() {
+
     // Common values for all operations
     var controlAccount = document.getElementById( "control-account" ).value;
     var symbol = "@@1234567";
 
-    // Gather fields for 'smt_create_operation'
-    var createOp = {
-        precision      : document.getElementById( "precision" ).value,
-        smtCreationFee : "",
-    };
 
-    // Gather fields for 'smt_set_setup_parameters_operation'
-    var setSetupParameters = {
-        allowVoting    : document.getElementById( "allow-voting" ).checked
-    };
+    var transaction = {};
+    transaction.operations = [];
 
-    // Gather fields for 'smt_set_runtime_parameters_operation'
-    var setRuntimeParameters = {
-        allowDownvoting               : document.getElementById( "allow-downvoting" ).checked,
-        cashoutWindowSeconds          : document.getElementById( "cashout-window-seconds" ).value,
-        reverseAuctionWindowSeconds   : document.getElementById( "reverse-auction-window-seconds" ).value,
-        voteRegenerationPeriodSeconds : document.getElementById( "vote-regeneration-period-seconds" ).value,
-        votesPerRegenerationPeriod    : document.getElementById( "votes-per-regeneration-period" ).value,
-        contentConstant               : document.getElementById( "content-constant" ).value,
-        percentCurationRewards        : document.getElementById( "percent-curation-rewards" ).value,
-        authorRewardCurve             : getRadiosValue( "author_reward_curve" ),
-        curationRewardCurve           : getRadiosValue( "curation_reward_curve" )
-    };
+    transaction.operations.push([
+        'smt_create', {
+             'control_account'  : controlAccount,
+             'symbol'           : symbol,
+             'precision'        : document.getElementById( "precision" ).value,
+             'smt_creation_fee' : ""
+        }
+    ]);
 
-    // Gather one or more emissions for 'smt_setup_emissions_operation'
-    var tokenEmissions = [];
+    transaction.operations.push([
+        'smt_set_setup_parameters', {
+             'control_account'  : controlAccount,
+             'symbol'           : symbol,
+             'allow_voting'     : document.getElementById( "allow-voting" ).checked
+        }
+    ]);
+
+    transaction.operations.push([
+        'smt_set_runtime_parameters', {
+            'control_account'                  : controlAccount,
+            'symbol'                           : symbol,
+            'allow_downvoting'                 : document.getElementById( "allow-downvoting" ).checked,
+            'cashout_window_seconds'           : document.getElementById( "cashout-window-seconds" ).value,
+            'reverse_auction_window_seconds'   : document.getElementById( "reverse-auction-window-seconds" ).value,
+            'vote_regeneration_period_seconds' : document.getElementById( "vote-regeneration-period-seconds" ).value,
+            'votes_per_regeneration_period'    : document.getElementById( "votes-per-regeneration-period" ).value,
+            'content_constant'                 : document.getElementById( "content-constant" ).value,
+            'percent_curation_rewards'         : document.getElementById( "percent-curation-rewards" ).value,
+            'author_reward_curve'              : getRadiosValue( "author_reward_curve" ),
+            'curation_reward_curve'            : getRadiosValue( "curation_reward_curve" )
+        }
+    ]);
+
     for ( i = 1; i <= _token_emission_ctr; i++ ) {
-        var tokenEmission = {
-            scheduleTime    : document.getElementById( "schedule-time-" + i ).value,
-            lepTime         : document.getElementById( "lep-time-" + i ).value,
-            repTime         : document.getElementById( "rep-time-" + i ).value,
-            intervalSeconds : document.getElementById( "interval-seconds-" + i ).value,
-            intervalCount   : document.getElementById( "interval-count-" + i ).value,
-            lepAbsAmount    : document.getElementById( "lep-abs-amount-" + i ).value,
-            repAbsAmount    : document.getElementById( "rep-abs-amount-" + i ).value,
-            lepRelNumerator : document.getElementById( "lep-rel-numerator-" + i ).value,
-            repRelNumerator : document.getElementById( "rep-rel-numerator-" + i ).value,
-            relAmtDenomBits : document.getElementById( "rel-amount-denom-bits-" + i ).value,
-            floorEmissions  : document.getElementById( "floor-emissions-" + i ).checked,
-            emissionsUnit   : document.getElementById( "emissions-unit-" + i ).value
-        };
-        tokenEmissions.push( tokenEmission );
+        transaction.operations.push([
+            'smt_setup_emissions', {
+                'control_account'       : controlAccount,
+                'symbol'                : symbol,
+                'schedule_time'         : document.getElementById( "schedule-time-" + i ).value,
+                'lep_time'              : document.getElementById( "lep-time-" + i ).value,
+                'rep_time'              : document.getElementById( "rep-time-" + i ).value,
+                'interval_seconds'      : document.getElementById( "interval-seconds-" + i ).value,
+                'interval_count'        : document.getElementById( "interval-count-" + i ).value,
+                'lep_abs_amount'        : document.getElementById( "lep-abs-amount-" + i ).value,
+                'rep_abs_amount'        : document.getElementById( "rep-abs-amount-" + i ).value,
+                'lep_rel_numerator'     : document.getElementById( "lep-rel-numerator-" + i ).value,
+                'rep_rel_numerator'     : document.getElementById( "rep-rel-numerator-" + i ).value,
+                'rel_amount_denom_bits' : document.getElementById( "rel-amount-denom-bits-" + i ).value,
+                'floor_emissions'       : document.getElementById( "floor-emissions-" + i ).checked,
+                'emissions_unit'        : document.getElementById( "emissions-unit-" + i ).value
+            }
+        ]);
     }
 
-    var setupOp = {
-        maxSupply             : document.getElementById( "max-supply" ).value,
-        contributionBeginTime : document.getElementById( "contribution-begin-time" ).value,
-        contributionEndTime   : document.getElementById( "contribution-end-time" ).value,
-        launchTime            : document.getElementById( "launch-time" ).value,
-        steemUnitsMin         : document.getElementById( "steem-units-min" ).value,
-        steemUnitsSoftCap     : document.getElementById( "steem-units-soft-cap" ).value,
-        steemUnitsHardCap     : document.getElementById( "steem-units-hard-cap" ).value,
-        initialGenerationPolicy : {
-            preSoftCapSteemUnit  : document.getElementById( "pre-soft-cap-steem-unit" ).value,
-            preSoftCapTokenUnit  : document.getElementById( "pre-soft-cap-token-unit" ).value,
-            postSoftCapSteemUnit : document.getElementById( "post-soft-cap-steem-unit" ).value,
-            postSoftCapTokenUnit : document.getElementById( "post-soft-cap-token-unit" ).value,
-            minUnitRatio         : document.getElementById( "min-unit-ratio" ).value,
-            maxUnitRatio         : document.getElementById( "max-unit-ratio" ).value
+    transaction.operations.push([
+        'smt_setup', {
+            'control_account'         : controlAccount,
+            'symbol'                  : symbol,
+            'max_supply'              : document.getElementById( "max-supply" ).value,
+            'contribution_begin_time' : document.getElementById( "contribution-begin-time" ).value,
+            'contribution_end_time'   : document.getElementById( "contribution-end-time" ).value,
+            'launch_time'             : document.getElementById( "launch-time" ).value,
+            'steem_units_min'         : document.getElementById( "steem-units-min" ).value,
+            'steem_units_soft_cap'     : document.getElementById( "steem-units-soft-cap" ).value,
+            'steem_units_hard_cap'     : document.getElementById( "steem-units-hard-cap" ).value,
+            'initial_generation_policy' : {
+                'pre_soft_cap_steem_unit'  : document.getElementById( "pre-soft-cap-steem-unit" ).value,
+                'pre_soft_cap_token_unit'  : document.getElementById( "pre-soft-cap-token-unit" ).value,
+                'post_soft_cap_steem_unit' : document.getElementById( "post-soft-cap-steem-unit" ).value,
+                'post_soft_cap_token_unit' : document.getElementById( "post-soft-cap-token-unit" ).value,
+                'min_unit_ratio'         : document.getElementById( "min-unit-ratio" ).value,
+                'max_unit_ratio'         : document.getElementById( "max-unit-ratio" ).value
+            }
         }
-    };
+    ]);
 
-    console.log( createOp );
-    console.log( setSetupParameters );
-    console.log( setRuntimeParameters );
-    console.log( tokenEmissions );
-    console.log( setupOp );
+    console.log( transaction );
 }
+
